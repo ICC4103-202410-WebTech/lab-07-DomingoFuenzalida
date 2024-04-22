4ea6fab036555fd8a4b54c8c4832e420dfa6477d
DomingoFuenzalida <dfuenzalida2@miuandes.cl>
2024-04-22 18:58:33 -0400

    arreglado los mensajes en user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-    
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    has_many :posts
+
+    validates :email, presence:{message: "Ponte un mail"}, uniqueness:{message: "Ya existe ese mail"}, format:{with: EMAIL_REGEX, message: "Escríbete bien ese Email!!"}
+    validates :name, presence: {message: "Acaso no tienes nombre?"}
+    validates :password, presence: {message: "Crea una contraseña"}, length: { minimum: 6, message: "La contraseña la tienes muy corta ;)"}
+
 end
